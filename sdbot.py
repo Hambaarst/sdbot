39895146fca916914c868a9faf14cee438beb3e8
Hambaarst <53864795+Hambaarst@users.noreply.github.com>
2023-08-03 11:28:23 +0300

    message content intents added

--- sdbot.py
+++ sdbot.py
@@ -8,11 +8,16 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
+intents.message_content=True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='¤', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello, {ctx.author.mention}!")
 
 bot.run(TOKEN)
