b44808e0a8efb2a8c4adc79188bb30ed6c377782
Hambaarst <53864795+Hambaarst@users.noreply.github.com>
2023-08-11 12:22:57 +0300

    send data moved

--- sdbot.py
+++ sdbot.py
@@ -12,7 +12,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.message_content=True
 
-url = "http://127.0.0.1:7860/sdapi/v1/txt2img"
+urlt2i = "http://127.0.0.1:7860/sdapi/v1/txt2img"
 
 message = "Hello, {ctx.author.mention}!"
 
@@ -31,14 +31,7 @@ async def test(ctx, *args):
     print(args)
     print(args[1])
 
-@bot.command()
-async def draw(ctx, *args):
-    if len(args) >= 2:
-        negative = args[1]
-    else:
-        negative = ""
-
-    send_data = {
+send_data = {
         "enable_hr": False,
         "denoising_strength": 0,
         "firstphase_width": 0,
@@ -51,7 +44,6 @@ async def draw(ctx, *args):
         "hr_sampler_name": None,
         "hr_prompt": None,
         "hr_negative_prompt": None,
-        "prompt": args[0],
         "styles": [""],
         "seed": -1,
         "subseed": -1,
@@ -59,7 +51,6 @@ async def draw(ctx, *args):
         "seed_resize_from_h": -1,
         "seed_resize_from_w": -1,
         "sampler_name": "UniPC",
-        "batch_size": 1,
         "n_iter": 1,
         "steps": 35,
         "cfg_scale": 7,
@@ -69,7 +60,6 @@ async def draw(ctx, *args):
         "tiling": False,
         "do_not_save_samples": False,
         "do_not_save_grid": False,
-        "negative_prompt": "BadDream FastNegativeV2 " + negative,
         "eta": 0,
         "s_min_uncond": 0,
         "s_churn": 0,
@@ -86,7 +76,17 @@ async def draw(ctx, *args):
         "alwayson_scripts": {}
         }
 
-    post_response = requests.post(url, json=send_data)
+@bot.command()
+async def draw(ctx, *args):
+    if len(args) >= 2:
+        negative = args[1]
+    else:
+        negative = ""
+
+    send_data["batch_size"] = 1
+    send_data["negative_prompt"] = "BadDream FastNegativeV2 " + negative
+
+    post_response = requests.post(urlt2i, json=send_data)
     post_response_json = post_response.json()
     files: list[discord.File] = []
     for image in post_response_json["images"]:
@@ -101,56 +101,10 @@ async def draw4(ctx, *args):
     else:
         negative = ""
 
-    send_data = {
-        "enable_hr": False,
-        "denoising_strength": 0,
-        "firstphase_width": 0,
-        "firstphase_height": 0,
-        "hr_scale": 2,
-        "hr_upscaler": None,
-        "hr_second_pass_steps": 0,
-        "hr_resize_x": 0,
-        "hr_resize_y": 0,
-        "hr_sampler_name": None,
-        "hr_prompt": None,
-        "hr_negative_prompt": None,
-        "prompt": args[0],
-        "styles": [""],
-        "seed": -1,
-        "subseed": -1,
-        "subseed_strength": 0,
-        "seed_resize_from_h": -1,
-        "seed_resize_from_w": -1,
-        "sampler_name": "UniPC",
-        "batch_size": 4,
-        "n_iter": 1,
-        "steps": 35,
-        "cfg_scale": 7,
-        "width": 512,
-        "height": 512,
-        "restore_faces": True,
-        "tiling": False,
-        "do_not_save_samples": False,
-        "do_not_save_grid": False,
-        "negative_prompt": "BadDream FastNegativeV2 " + negative,
-        "eta": 0,
-        "s_min_uncond": 0,
-        "s_churn": 0,
-        "s_tmax": 0,
-        "s_tmin": 0,
-        "s_noise": 1,
-        "override_settings": {},
-        "override_settings_restore_afterwards": True,
-        "script_args": [],
-        "sampler_index": "UniPC",
-        "script_name": None,
-        "send_images": True,
-        "save_images": False,
-        "alwayson_scripts": {}
-        }
-
-
-    post_response = requests.post(url, json=send_data)
+    send_data["batch_size"] = 4
+    send_data["negative_prompt"] = "BadDream FastNegativeV2 " + negative
+    
+    post_response = requests.post(urlt2i, json=send_data)
     post_response_json = post_response.json()
     files: list[discord.File] = []
     for image in post_response_json["images"]:
