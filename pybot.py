47eacb2e2207072bf610ecdbd47b8e5adbadfd95
Andreas Tali <53864795+Hambaarst@users.noreply.github.com>
2023-08-29 12:08:50 +0300

    variation buttons working

--- pybot.py
+++ pybot.py
@@ -13,50 +13,39 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.message_content=True
 urlt2i = "http://127.0.0.1:7860/sdapi/v1/txt2img"
+urli2i = "http://127.0.0.1:7860/sdapi/v1/img2img"
 send_data = {
-        "enable_hr": False,
-        "denoising_strength": 0,
-        "firstphase_width": 0,
-        "firstphase_height": 0,
-        "hr_scale": 2,
-        "hr_upscaler": None,
-        "hr_second_pass_steps": 0,
-        "hr_resize_x": 0,
-        "hr_resize_y": 0,
-        "hr_sampler_name": None,
-        "hr_prompt": None,
-        "hr_negative_prompt": None,
-        "styles": [""],
-        "seed": -1,
-        "subseed": -1,
-        "subseed_strength": 0,
-        "seed_resize_from_h": -1,
-        "seed_resize_from_w": -1,
-        "sampler_name": "UniPC",
+        "sampler_name": "DPM++ SDE Karras",
         "n_iter": 1,
         "steps": 35,
         "cfg_scale": 7,
         "width": 512,
         "height": 512,
-        "restore_faces": True,
-        "tiling": False,
-        "do_not_save_samples": False,
-        "do_not_save_grid": False,
-        "eta": 0,
-        "s_min_uncond": 0,
-        "s_churn": 0,
-        "s_tmax": 0,
-        "s_tmin": 0,
-        "s_noise": 1,
-        "override_settings": {},
-        "override_settings_restore_afterwards": True,
-        "script_args": [],
-        "sampler_index": "UniPC",
-        "script_name": None,
+        "sampler_index": "DPM++ SDE Karras",
         "send_images": True,
-        "save_images": False,
-        "alwayson_scripts": {}
+        "save_images": False
         }
+
+i2idata = {
+  "init_images": [],
+  "resize_mode": 1,
+  "denoising_strength": 0.48,
+  "prompt": "",
+  "sampler_name": "DPM++ SDE Karras",
+  "batch_size": 1,
+  "n_iter": 4,
+  "steps": 35,
+  "cfg_scale": 7,
+  "width": 512,
+  "height": 512,
+  "negative_prompt": "",
+  "sampler_index": "DPM++ SDE Karras",
+  "include_init_images": False,
+  "script_name": None,
+  "send_images": True,
+  "save_images": False,
+  "alwayson_scripts": {}
+}
 
 bot = discord.Bot(intents=intents)
 
@@ -94,7 +83,7 @@ async def draw(ctx: discord.Interaction, prompt: str = "", negatives: str = "", 
     for image in post_response_json["images"]:
         img = base64.b64decode(image)
         files.append(discord.File(io.BytesIO(img),filename=f"img{i}.png"))
-        button = Button(label=f"image {i}", style=discord.ButtonStyle.blurple)
+        button = ImageButton(image=img, send_data=send_data, label=f"image {i}", style=discord.ButtonStyle.blurple)
         view.add_item(button)
         i = i + 1
 
@@ -102,8 +91,53 @@ async def draw(ctx: discord.Interaction, prompt: str = "", negatives: str = "", 
 
 async def img2img(ctx: discord.Interaction, image, send_data):
     await ctx.response.defer(invisible=False)
+    i2idata["init_images"] = [image]
+    i2idata["prompt"] = send_data["prompt"]
+    i2idata["negative_prompt"] = send_data["negative_prompt"]
+    post_response = requests.post(urli2i, json=i2idata)
+    post_response_json = post_response.json()
+    i: int = 1
+    files:list[discord.File] = []
+    view = View()
+    for image in post_response_json["images"]:
+        img = base64.b64decode(image)
+        files.append(discord.File(io.BytesIO(img),filename=f"img{i}.png"))
+        button = ImageButton(image=image, send_data=send_data, label=f"image {i}", style=discord.ButtonStyle.blurple)
+        view.add_item(button)
+        i = i + 1
+    await ctx.followup.send(files=files, view=view)
 
+class ImageButton(discord.ui.Button):
+    def __init__(self, image, send_data, *args, **kwargs):
+        super().__init__(
+            *args,
+            **kwargs
+        )
+        self.image = image
+        self.send_data = send_data
 
+    async def callback(self, interaction: discord.Interaction):
+        
+        await interaction.response.defer()
+        image_data = base64.b64encode(self.image).decode("utf-8")
+        
+        i2idata["init_images"]=['data:image/png;base64,' + image_data]
+        i2idata["negative_prompt"]=self.send_data["negative_prompt"]
+        i2idata["prompt"]: self.send_data["prompt"]
+        post_response = requests.post(urli2i, json=i2idata)
+        post_response_json = post_response.json()
+        i: int = 1
+        files:list[discord.File] = []
+        view = View()
+        
+        for image in post_response_json["images"]:
+            img = base64.b64decode(image)
+            files.append(discord.File(io.BytesIO(img),filename=f"img{i}.png"))
+            button = ImageButton(image=img, send_data=send_data, label=f"image {i}", style=discord.ButtonStyle.blurple)
+            view.add_item(button)
+            i = i + 1
+
+        await interaction.followup.send(files=files, view=view)
 
 @bot.event
 async def on_message(msg):
